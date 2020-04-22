4097148c5cb8d1763e4c6e5b0d3cc7da2df128e5
gtossa <gtossa@lafourchette.com>
2020-04-22 11:52:36 +0200

    Implement jupyter connector for snowflake

---
+++ snowflakes/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+with open('requirements.txt') as f:
+      content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='snowflakes_magic',
+      version='1.0',
+      description='Jupyter Connector to Snowflake',
+      author='Gildas Darex TOSSA',
+      author_email='gtossa@lafourchette.com',
+      url='none',
+      install_requires=requirements
+     )
+
